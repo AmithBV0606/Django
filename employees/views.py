48b175a3594fe8a6f96950e37579903808ee4cff
Amith B V <amithrao0606@gmail.com>
2025-08-12 17:54:22 +0530

    Fetch employee details.

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import  HttpResponse
+from django.shortcuts import render, get_object_or_404
+from employees.models import Employee
 
 # Create your views here.
+
+
+def employee_details(request, pk):
+    # print(pk)
+    # try:
+    #     employee = Employee.objects.get(pk=pk)
+    #     print(employee)
+    #     # context = {
+    #     #     'employee': employee
+    #     # }
+    #     # return render(request, 'home.html', context)
+    # except:
+    #     raise Http404
+
+    employee = get_object_or_404(Employee, pk=pk)
+    return HttpResponse(employee)
